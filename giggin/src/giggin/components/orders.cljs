d8dccd2707ec06d33283db0a1e58c29ef8b37f28
Norman Kabir <nkabir@gmail.com>
2021-04-17 15:57:56 -0500

    22-subscribe

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -1,7 +1,8 @@
 (ns giggin.components.orders
   (:require [giggin.state :as state]
             [giggin.helpers :refer [format-price]]
-            [giggin.components.checkout-modal :refer [checkout-modal]]))
+            [giggin.components.checkout-modal :refer [checkout-modal]]
+            [giggin.components.admin-panel :refer [admin-panel]]))
 
 (defn total
   []
@@ -14,25 +15,26 @@
   (let [remove-from-order #(swap! state/orders dissoc %)
         remove-all-orders #(reset! state/orders {})]
     [:aside
+     [admin-panel]
      (if (empty? @state/orders)
        [:div.empty
         [:div.title "You don't have any orders"]
         [:div.subtitle "Click on a + to add an order"]]
        [:div.order
         [:div.body
-         (for [[id quant] @state/orders]
-           [:div.item {:key id}
-            [:div.img
-             [:img {:src (get-in @state/gigs [id :img])
-                    :alt (get-in @state/gigs [id :title])}]]
-            [:div.content
-             [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
-            [:div.action
-             [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
-             [:button.btn.btn--link.tooltip
-              {:data-tooltip "Remove"
-               :on-click #(remove-from-order id)}
-              [:i.icon.icon--cross]]]])]
+         (doall (for [[id quant] @state/orders]
+                  [:div.item {:key id}
+                   [:div.img
+                    [:img {:src (get-in @state/gigs [id :img])
+                           :alt (get-in @state/gigs [id :title])}]]
+                   [:div.content
+                    [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+                   [:div.action
+                    [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
+                    [:button.btn.btn--link.tooltip
+                     {:data-tooltip "Remove"
+                      :on-click #(remove-from-order id)}
+                     [:i.icon.icon--cross]]]]))]
         [:div.total
          [:hr]
          [:div.item
--- giggin/src/giggin/fb/init.cljs
+++ giggin/src/giggin/fb/init.cljs
@@ -2,11 +2,13 @@
   (:require ["firebase/app" :as firebase]
             ["firebase/database"]
             ["firebase/auth"]))
-
+            
 (defn firebase-init
   []
-  (firebase/initializeApp
-   {:apiKey "your-api-key"
-    :authDomain "your-auth-domain"
-    :databaseURL "your-databse-url"
-    :projectId "your-project-id"}))
+  (if (zero? (alength firebase/apps))
+    (firebase/initializeApp
+     #js {:apiKey "your-api-key"
+          :authDomain "your-auth-domain"
+          :databaseURL "your-databse-url"
+          :projectId "your-project-id"})
+    (firebase/app)))
