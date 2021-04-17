414da6e7a3937cc1bb2d6ca10737a8dd7a6b2171
Norman Kabir <nkabir@gmail.com>
2021-04-17 17:00:13 -0500

    updating code

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -3,8 +3,9 @@
 
 (defn gigs
   []
-  [:main
+  [:main ;; main element <main>
    [:div.gigs (map (fn [gig]
+                     ;; queries are against properties stored in state objects
                      [:div.gig {:key (:id gig)}
                       [:img.gig__artwork {:src (:img gig) :alt (:title gig)}]
                       [:div.gig__body
@@ -12,4 +13,5 @@
                         [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
                          [:i.icon.icon--plus]] (:title gig)]
                        [:p.gig__price (:price gig)]
+                       ;; derefernce atom @state/gigs
                        [:p.gig__desc (:desc gig)]]]) (vals @state/gigs))]])
